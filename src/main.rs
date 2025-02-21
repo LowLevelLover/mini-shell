a911a4397a6a8d6e65737d3ce75207b52e897dda
Farzin Vatani <farzinvatani@outlook.com>
2025-02-21 19:56:30 +0330

    Add exit command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
+
+struct Command<'a> {
+    command: &'a str,
+    args: Option<Vec<&'a str>>,
+}
+
+impl<'a> Command<'a> {
+    fn new(input: &'a str) -> Self {
+        if let Some(index) = input.find(' ') {
+            Self {
+                command: &input[..index],
+                args: Some(Vec::from_iter(input[index..].split_whitespace())),
+            }
+        } else {
+            Self {
+                command: input,
+                args: None,
+            }
+        }
+    }
+
+    fn run(&self) {
+        match self.command {
+            "exit" => {
+                let code = self.args.as_ref().unwrap_or(&vec!["0"])[0]
+                    .parse::<i32>()
+                    .unwrap();
+                process::exit(code);
+            }
+            _ => {
+                println!("{}: command not found", self.command.trim_end());
+            }
+        }
+    }
+}
 
 fn main() {
     loop {
@@ -9,6 +45,11 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim_end());
+        input = input.trim().to_owned();
+
+        if input.len() != 0 {
+            let cmd = Command::new(&input);
+            cmd.run();
+        }
     }
 }
