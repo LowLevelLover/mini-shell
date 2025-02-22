7a5fa7491ddd1c3284a95eed34679b784746e975
Farzin Vatani <farzinvatani@outlook.com>
2025-02-22 03:55:22 +0330

    Refactor code to improve readablity

---
+++ src/lib.rs
@@ -0,0 +1,102 @@
+use std::{env, fs, process, sync::OnceLock};
+
+static CACHE: OnceLock<Vec<fs::DirEntry>> = OnceLock::new();
+static COMMANDS: [&str; 3] = ["exit", "echo", "type"];
+
+pub enum Commands<'a> {
+    Unknown(&'a str),
+    Exit(i32),
+    Echo(&'a str),
+    Type(&'a str),
+    External { command: &'a str, args: &'a str },
+}
+
+impl<'a> Commands<'a> {
+    pub fn parse(input_raw: &'a str) -> Self {
+        let command: &str;
+        let mut args_raw: Option<&str> = None;
+
+        if let Some(index) = input_raw.find(' ') {
+            command = &input_raw[..index];
+            args_raw = Some(&input_raw[index..]);
+        } else {
+            command = &input_raw;
+        }
+
+        match command {
+            "exit" => Self::Exit(
+                args_raw
+                    .unwrap_or(&"0")
+                    .trim_start()
+                    .parse::<i32>()
+                    .unwrap(),
+            ),
+            "echo" => Self::Echo(args_raw.unwrap_or("").trim_start()),
+            "type" => Self::Type(args_raw.unwrap_or("type")),
+            input => {
+                if Self::find_ext_command(command).is_some() {
+                    Self::External {
+                        command,
+                        args: args_raw.unwrap_or("").trim(),
+                    }
+                } else {
+                    Self::Unknown(input)
+                }
+            }
+        }
+    }
+
+    pub fn exec(self) {
+        match self {
+            Self::Unknown(cmd) => println!("{}: command not found", cmd.trim_end()),
+            Self::Exit(code) => process::exit(code),
+            Self::Echo(text) => println!("{}", text),
+            Self::Type(cmd) => {
+                if COMMANDS.contains(&cmd) {
+                    println!("{} is a shell builtin", cmd);
+                } else if let Some(entry) = Self::find_ext_command(cmd) {
+                    println!("{} is {}", cmd, entry.path().to_str().unwrap());
+                } else {
+                    println!("{}: not found", cmd);
+                }
+            }
+            Self::External { command, args } => {
+                let args = if args.chars().count() == 0 {
+                    None
+                } else {
+                    Some(args)
+                };
+                let output = process::Command::new(command)
+                    .args(args)
+                    .output()
+                    .expect("Failed to execute command");
+
+                let stdout = String::from_utf8(output.stdout).expect("Failed to read output");
+                print!("{}", stdout);
+            }
+        }
+    }
+
+    fn find_ext_command(target: &str) -> Option<&fs::DirEntry> {
+        let ext_commands = CACHE.get_or_init(get_ext_commands);
+        ext_commands
+            .iter()
+            .filter(|entry| entry.file_name().eq(target))
+            .next()
+    }
+}
+
+fn get_ext_commands() -> Vec<fs::DirEntry> {
+    let pathes = env::var("PATH").unwrap();
+    let dirs: Vec<String> = pathes.split(":").map(|el| el.to_string()).collect();
+
+    let mut commands = Vec::new();
+
+    for dir in dirs.iter() {
+        if let Ok(entries) = fs::read_dir(dir) {
+            commands.extend(entries.flatten());
+        }
+    }
+
+    commands
+}
--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,7 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{env, fs, path::PathBuf, process};
 
-struct Command<'a> {
-    command: &'a str,
-    args: Option<&'a str>,
-}
-
-impl<'a> Command<'a> {
-    fn new(input: &'a str) -> Self {
-        if let Some(index) = input.find(' ') {
-            Self {
-                command: &input[..index],
-                args: Some(&input[index + 1..]),
-            }
-        } else {
-            Self {
-                command: input,
-                args: None,
-            }
-        }
-    }
-
-    fn run(&self) {
-        match self.command {
-            "exit" => {
-                let code = if let Some(arg) = self.args {
-                    arg.parse::<i32>().unwrap()
-                } else {
-                    0
-                };
-                process::exit(code);
-            }
-            "echo" => {
-                if let Some(text) = self.args {
-                    println!("{}", text);
-                } else {
-                    println!();
-                }
-            }
-            "type" => {
-                static COMMANDS: [&str; 3] = ["exit", "echo", "type"];
-                if let Some(text) = self.args {
-                    if COMMANDS.contains(&text) {
-                        println!("{} is a shell builtin", text);
-                    } else {
-                        if let Some(path) = Command::find_ex_command(text) {
-                            println!("{} is {}", self.args.unwrap(), path.to_str().unwrap());
-                        } else {
-                            println!("{}: not found", self.args.unwrap())
-                        }
-                    }
-                }
-            }
-            _ => {
-                if Command::find_ex_command(self.command).is_some() {
-                    let output = process::Command::new(self.command)
-                        .args(self.args)
-                        .output()
-                        .expect("Failed to execute command");
-
-                    let stdout = String::from_utf8(output.stdout).expect("Failed to read output");
-                    print!("{}", stdout);
-                } else {
-                    println!("{}: command not found", self.command.trim_end());
-                }
-            }
-        }
-    }
-
-    fn find_ex_command(target: &str) -> Option<PathBuf> {
-        let path = env::var("PATH").unwrap();
-        let dirs: Vec<&str> = path.split(":").collect();
-
-        for dir in dirs.iter() {
-            if let Ok(entries) = fs::read_dir(dir) {
-                for entry in entries {
-                    let entry = entry.unwrap();
-                    if entry.file_name().eq(target) {
-                        return Some(entry.path());
-                    }
-                }
-            }
-        }
-        None
-    }
-}
+use codecrafters_shell::Commands;
 
 fn main() {
     loop {
@@ -97,9 +13,9 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
         input = input.trim().to_owned();
 
-        if input.len() != 0 {
-            let cmd = Command::new(&input);
-            cmd.run();
+        if input.chars().count() != 0 {
+            let cmd = Commands::parse(&input);
+            cmd.exec();
         }
     }
 }
